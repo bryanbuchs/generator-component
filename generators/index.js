14c0b55ba8bfbf6cba52356dcd79ab571955407f
Bryan Buchs <bryan@kapowinc.com>
2023-12-21 16:27:02 -0600

    eliminate passing options from cli, prompt required

--- generators/index.js
+++ generators/index.js
@@ -1,107 +1,77 @@
 import Generator from 'yeoman-generator'
-import converter from 'number-to-words'
 import { capitalCase, kebabCase, pascalCase, sentenceCase } from 'change-case'
 
 export default class GeneratorTwigComponent extends Generator {
-  constructor(args, opts) {
-    super(args, opts)
-    this.argument('tag', { type: String, required: false })
-    this.argument('count', { type: Number, required: false, default: 0 }) // the number of additional stories to generate
-    this.option('js', { type: Boolean, default: false }) // build a behavior.js file
-    this.option('padding', { type: Boolean, default: false }) // set storybook preview paddings on/off
-    this.option('decorator', { type: Boolean, default: false }) // wrap the story in a container
-    this.option('group', { type: String, required: false, default: '(None)' }) // set a default storybook group
+  async prompting () {
+    this.answers = await this.prompt([
+      {
+        type: 'input',
+        name: 'tag',
+        message: 'Name of component ["card"]'
+      },
+      {
+        type: 'list',
+        name: 'group',
+        message: 'Component type',
+        choices: [
+          'Block',
+          'Content',
+          'Entity',
+          'Field',
+          'Global',
+          'Media',
+          'Nav',
+          'Node',
+          'OTHER',
+          'Page',
+          'Paragraph',
+          'Region',
+          'View',
+          'Widget',
+          '(None)'
+        ],
+        default: 0
+      },
+      {
+        type: 'input',
+        name: 'description'
+      },
+      {
+        type: 'input',
+        name: 'stories',
+        message:
+          'names of additional stories, comma-separated ["Default, Secondary"]'
+      },
+      {
+        type: 'checkbox',
+        name: 'parameters',
+        message: 'Storybook options',
+        choices: [
+          {
+            name: 'Remove paddings',
+            value: 'paddings',
+            short: 'no-paddings'
+          },
+          {
+            name: 'Add decorator',
+            value: 'decorator',
+            short: 'add-wrapper'
+          }
+        ]
+      },
+      {
+        type: 'confirm',
+        name: 'js',
+        message: 'Include *.behavior.js file?',
+        default: false
+      }
+    ])
   }
 
-  async prompting() {
-    if (!('tag' in this.options)) {
-      this.answers = await this.prompt([
-        {
-          type: 'input',
-          name: 'tag',
-          message: 'Name of component ["image", "cards"]'
-        },
-        {
-          type: 'list',
-          name: 'group',
-          message: 'Component type',
-          choices: [
-            '(None)',
-            'Block',
-            'Content',
-            'Entity',
-            'Field',
-            'Global',
-            'Media',
-            'Nav',
-            'Node',
-            'Page',
-            'Paragraph',
-            'Region',
-            'View',
-            'Widget',
-            'OTHER'
-          ],
-          default: 0
-        },
-        {
-          type: 'input',
-          name: 'description'
-        },
-        {
-          type: 'input',
-          name: 'stories',
-          message:
-            'names of additional stories, comma-separated [Default, Secondary]'
-        },
-        {
-          type: 'checkbox',
-          name: 'parameters',
-          message: 'Storybook options',
-          choices: [
-            {
-              name: 'Remove paddings',
-              value: 'paddings',
-              short: 'no-paddings'
-            },
-            {
-              name: 'Add decorator',
-              value: 'decorator',
-              short: 'add-wrapper'
-            }
-          ]
-        },
-        {
-          type: 'confirm',
-          name: 'js',
-          message: 'Include *.behavior.js file?',
-          default: false
-        }
-      ])
-    } else {
-      this.answers = this.options
-      this.answers.stories = null
-      this.answers.parameters = []
-      if (this.options.padding) {
-        this.answers.parameters.push('paddings')
-      }
-      if (this.options.decorator) {
-        this.answers.parameters.push('decorator')
-      }
-      if (this.options.count !== 0) {
-        this.answers.stories = Array.from(Array(this.options.count).keys())
-          .map((i) => {
-            return converter.toWords(i + 1)
-          })
-          .join(',')
-      }
-    }
-  }
-
-  writing() {
+  writing () {
     const pkg = this.fs.readJSON(`${this.contextRoot}/package.json`)
 
-    // => "List Items", "Button"m "Page Title"
+    // => "List Items", "Button", "Page Title"
     const str = this.answers.tag.replaceAll('-', ' ')
 
     const group = this.answers.group !== '(None)' ? this.answers.group : null
@@ -125,7 +95,7 @@ export default class GeneratorTwigComponent extends Generator {
         ? `${this.answers.group}/${capitalCase(str)}`
         : capitalCase(str),
       stories: this.answers.stories
-        ? this.answers.stories.split(',').map((name) => {
+        ? this.answers.stories.split(',').map(name => {
             return {
               name: pascalCase(name.trim()),
               label: capitalCase(name.trim())
@@ -145,7 +115,7 @@ export default class GeneratorTwigComponent extends Generator {
 
     this.destinationRoot(`components/${props.tag}`)
 
-    templates.forEach((ext) => {
+    templates.forEach(ext => {
       this.fs.copyTpl(
         this.templatePath(`component.${ext}`),
         this.destinationPath(`${props.tag}.${ext}`),
