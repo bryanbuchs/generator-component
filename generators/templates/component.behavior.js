761a2c58db6dfd1004665a38e1f8f1234b02c5f7
Bryan Buchs <bryan@kapowinc.com>
2024-06-10 10:03:49 -0500

    Output placholder rules for each field in LESS template

--- generators/templates/component.behavior.js
+++ generators/templates/component.behavior.js
@@ -7,7 +7,7 @@ Drupal.behaviors.<%= name %> = {
   attach: function (context) {
     const elements = once('<%= name _%>', '.<%= tag _%>', context)
     elements.forEach(<%= forEach %> => {
-      // do stuff with <%= forEach %>
+      // ...
     })
   }
 }
